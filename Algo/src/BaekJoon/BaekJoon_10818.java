998d339c4a03fb286ba01156b5c67835d7d00f1b
movingyun <97611103+movingyun@users.noreply.github.com>
2022-01-14 17:37:50 +0900

    Add files via upload

---
+++ Algo/src/BaekJoon/BaekJoon_10818.java
@@ -0,0 +1,23 @@
+package BaekJoon;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import javax.print.DocFlavor.BYTE_ARRAY;
+
+public class BaekJoon_10818 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n= sc.nextInt();
+		int[] arr = new int[n];
+				
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);		
+		System.out.println(arr[0] + " " + arr[n-1]);
+	}
+}
