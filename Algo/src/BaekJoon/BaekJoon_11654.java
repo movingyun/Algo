c68bc4e301462fe9c58ba0cfefb2146993d4a0e8
Dongyun You <ydy1107@naver.com>
2022-01-26 22:20:46 +0900

    22.01.26

---
+++ Algo/src/BaekJoon/BaekJoon_11654.java
@@ -0,0 +1,12 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BaekJoon_11654 {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	String a = sc.next();
+	System.out.println((int)a.charAt(0));
+}
+}
