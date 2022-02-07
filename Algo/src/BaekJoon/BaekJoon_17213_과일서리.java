dbb43371c2532587a0d4ec110555ad436541b806
Dongyun You <ydy1107@naver.com>
2022-02-07 22:36:17 +0900

    22.02.07

---
+++ Algo/src/BaekJoon/BaekJoon_17213_과일서리.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BaekJoon_17213_과일서리 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+
+		int result = n * (n - 1) * (m - n - 1);
+
+		if (n!=1 && n!=m)
+			System.out.println(result);
+		else
+			System.out.println(1);
+
+	}
+}
---
+++ Algo/src/BaekJoon/BaekJoon_4153.java
@@ -0,0 +1,21 @@
+package BaekJoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BaekJoon_4153 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		for (;;) {
+			int[] arr = { sc.nextInt(), sc.nextInt(), sc.nextInt() };
+			Arrays.sort(arr);
+			if (arr[0] + arr[1] + arr[2] == 0)
+				break;
+			if (Math.pow(arr[0], 2) + Math.pow(arr[1], 2) == Math.pow(arr[2], 2))
+				System.out.println("right");
+			else
+				System.out.println("wrong");
+		}
+	}
+}
