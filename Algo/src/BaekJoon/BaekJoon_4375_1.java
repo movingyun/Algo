a92a2d9e7adc18137531c69db593e558f41f4d02
Dongyun You <ydy1107@naver.com>
2022-02-12 21:47:33 +0900

    22.02.12

--- Algo/src/BaekJoon/BaekJoon_4375_1.java
+++ Algo/src/BaekJoon/BaekJoon_4375_1.java
@@ -1,28 +1,23 @@
 package BaekJoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class BaekJoon_4375_1 {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while (true) {
-			String s = br.readLine();
-			if (s == null) {
-				break;
-			}
-			int N = Integer.parseInt(s);
-			int num = 0;
-			for (int i = 1; i <= N; i++) {
-				num = num * 10 + 1;
-				num %= N;
-				System.out.println(num);
-				if (num == 0) {
-					System.out.println(i);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNextInt()) {
+			int n =sc.nextInt();
+			int a = 0;
+			int count =0;
+			for(int i=1; i<=n; i++) {
+				a = (a*10+1)%n;
+				count++;
+				if(a==0) {
 					break;
 				}
 			}
+			System.out.println(count);			
 		}
+
 	}
 }
--- Algo/src/BaekJoon/BaekJoon_7568_덩치.java
+++ Algo/src/BaekJoon/BaekJoon_7568_덩치.java
@@ -1,5 +1,29 @@
 package BaekJoon;
 
-public class BaekJoon_7568_덩치 {
+import java.util.Scanner;
 
+public class BaekJoon_7568_덩치 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int[][] arr = new int[a][2];
+
+		for (int i = 0; i < a; i++) {// 몸무게, 키 입력
+			for (int j = 0; j < 2; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+
+		for (int q = 0; q < a; q++) {
+			int sum = 0;
+			for (int k = 0; k < a; k++) {
+				if (q == k)
+					continue;
+				if (arr[q][0] < arr[k][0] && arr[q][1] < arr[k][1])
+					sum += 1;
+			}
+			System.out.println(sum + 1);
+		}
+	}
 }
