91d266ed6cebf6a91f75928c2c9592d87d7683ca
Dongyun You <ydy1107@naver.com>
2022-03-07 23:51:33 +0900

    22.03.07

---
+++ Algo/src/BaekJoon/BaekJoon_5554_B5_심부름가는길.java
@@ -0,0 +1,16 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BaekJoon_5554_B5_심부름가는길 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		int result = a + b + c + d;
+		System.out.println(result / 60);
+		System.out.println(result % 60);
+	}
+}
