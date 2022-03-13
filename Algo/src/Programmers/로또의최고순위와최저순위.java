68dbca6332e8e301e180eb75888405a24145b714
Dongyun You <ydy1107@naver.com>
2022-03-13 23:10:49 +0900

    22.03.13

---
+++ Algo/src/Programmers/로또의최고순위와최저순위.java
@@ -0,0 +1,45 @@
+package Programmers;
+
+import java.util.Scanner;
+
+public class 로또의최고순위와최저순위 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] lottos = new int[6];
+		for (int i = 0; i < 6; i++) {
+			lottos[i] = sc.nextInt();
+		}
+		int[] win_nums = new int[6];
+		for (int i = 0; i < 6; i++) {
+			win_nums[i] = sc.nextInt();
+		}
+		int zero = 0;
+		int score = 1;
+		for (int i = 0; i < 6; i++) {// 내가 선택
+			int cnt = 0;
+			if (lottos[i] == 0) {
+				zero++;
+				continue;
+			}
+			for (int j = 0; j < 6; j++) {// 답지
+				if (lottos[i] == win_nums[j]) {
+					cnt++;
+					break;
+				}
+			}
+			if (cnt == 0)
+				score++;
+		}
+		if (score > 6)
+			score = 6;
+
+		int max = score;
+		int min = score + zero;
+
+		if (score > 6)
+			score = 6;
+		
+		System.out.println(max);
+		System.out.println(min);
+	}
+}
